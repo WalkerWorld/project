04af4f0395c7f9e1ae1b0faa236bd61d29566f41
walker <walker@172.16.88.7>
2017-03-09 20:33:18 +0800

    jdjf

---
+++ Utils/src/com/walker/jutil/TypeUtil.java
@@ -0,0 +1,47 @@
+/**   
+* @Title: TypeUtil.java
+* @Package com.walker.jutil
+* @Description: TODO
+* @author A18ccms A18ccms_gmail_com   
+* @date 2017年3月9日 下午6:55:05
+* @version V1.0   
+*/
+package com.walker.jutil;
+
+import java.text.DecimalFormat;
+
+/** @ClassName: TypeUtil
+ *
+ * @Description: TODO
+ *
+ * @author walker
+ *
+ * @date 2017年3月9日 下午6:55:05
+ * 
+ */
+public class TypeUtil {
+	
+	/**
+	 * Add by walker Date 2017年3月9日
+	 * @Description: TODO
+	 * 	String 类型转换成double数字 
+	 *  @param str 被转换字符串
+	 *  @return 如果是合法数字字符串时返回对应数字，否则返回0
+	 */
+	public static double strToDouble(String str){
+		try {
+			return Double.parseDouble(str);
+		} catch (Exception e) {
+			return 0;
+		}
+	}
+	
+	
+	public static String getNumStr(double num){
+		DecimalFormat decimalFormat = new DecimalFormat("#0.00");
+		return decimalFormat.format(num);
+		
+//		return String.format("%.2f", num);
+	}
+	
+}
---
+++ Utils/src/com/walker/test/mainTest.java
@@ -0,0 +1,29 @@
+/**   
+* @Title: mainTest.java
+* @Package com.walker.test
+* @Description: TODO
+* @author A18ccms A18ccms_gmail_com   
+* @date 2017年3月9日 下午6:56:36
+* @version V1.0   
+*/
+package com.walker.test;
+
+import org.junit.Test;
+
+import com.walker.jutil.TypeUtil;
+
+/** @ClassName: mainTest
+ *
+ * @Description: TODO
+ *
+ * @author walker
+ *
+ * @date 2017年3月9日 下午6:56:36
+ * 
+ */
+public class mainTest {
+	@Test
+	public void test(){
+		System.out.println("======" + TypeUtil.getNumStr(00000002.126423423423423));
+	}
+}
